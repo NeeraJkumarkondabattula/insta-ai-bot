755d2f62381904e06de4395359a131e3ae1c38d6
NeeraJkumarkondabattula <neerajkumarkondabattula@gmail.com>
2025-08-02 20:53:09 +0530

    updated

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.get("/webhook", (req, res) => {
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("Webhook verified");
-    res.status(200).send(challenge);
+  if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+    console.log("Webhook verified!");
+    res.status(200).send(challenge); // <-- Important!
   } else {
     res.sendStatus(403);
   }
